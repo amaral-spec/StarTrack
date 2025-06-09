384ab41614749225cf47aaaee0471d7a11e88ff3
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-06-09 10:52:24 -0300

    launcher folder

---
+++ StarTrack/StarTrack/App/Launcher/StarTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarTrackApp.swift
+//  StarTrack
+//
+//  Created by Gabriel Rugeri on 09/06/25.
+//
+
+import SwiftUI
+
+@main
+struct StarTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
