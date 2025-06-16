1f64a0f75dc39728d194801f584b4c08cd7f4e14
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-06-16 10:22:01 -0300

    added base files

---
+++ StarTrack/StarTrack/Services/Supabase/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  StarTrack
+//
+//  Created by Gabriel Rugeri on 16/06/25.
+//
+
+import Supabase
+import SwiftUI
+
+struct ContentView: View {
+  @State var todos: [Todo] = []
+
+  var body: some View {
+	NavigationStack {
+	  List(todos) { todo in
+		Text(todo.title)
+	  }
+	  .navigationTitle("Todos")
+	  .task {
+		do {
+		  todos = try await supabase.from("todos").select().execute().value
+		} catch {
+		  debugPrint(error)
+		}
+	  }
+	}
+  }
+}
+
+#Preview {
+  ContentView()
+}
+
---
+++ StarTrack/StarTrack/Services/Supabase/Todo.swift
@@ -0,0 +1,14 @@
+//
+//  Todo.swift
+//  StarTrack
+//
+//  Created by Gabriel Rugeri on 16/06/25.
+//
+
+import Foundation
+
+struct Todo: Identifiable, Decodable {
+  var id: Int
+  var title: String
+}
+
