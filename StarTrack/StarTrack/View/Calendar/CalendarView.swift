2ee86976b9ae33a7581b205703ba8c0ba320aaf2
Fábio Ribeiro <104939520+Fab1o98@users.noreply.github.com>
2025-06-18 10:38:25 -0300

    Telas iniciais do projeto, TabBar, Catalogo, Calendario e Observatorios

---
+++ StarTrack/StarTrack/View/Calendar/CalendarView.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarView.swift
+//  StarTrack
+//
+//  Created by Aluno 10 on 16/06/25.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    var body: some View {
+        Text("Page Calendar")
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+        TabBar()
+    }
+}
---
+++ StarTrack/StarTrack/View/Observatories/ObservatoriesView.swift
@@ -0,0 +1,21 @@
+//
+//  ObservatoriesView.swift
+//  StarTrack
+//
+//  Created by Aluno 10 on 16/06/25.
+//
+
+import SwiftUI
+
+struct ObservatoriesView: View {
+    var body: some View {
+        Text("Page Observatories")
+    }
+}
+
+struct ObservatoriesView_Previews: PreviewProvider {
+    static var previews: some View {
+        ObservatoriesView()
+        TabBar()
+    }
+}
