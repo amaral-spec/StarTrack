9e489427cf55b5339c9cb269bde7183e5f903e9e
Gabriel Amaral <gabrielamaral1301@gmail.com>
2025-06-25 11:38:52 -0300

    Tela de observatórios

---
+++ StarTrack/StarTrack/Model/Observatory/Observatory.swift
@@ -0,0 +1,8 @@
+//
+//  Observatory.swift
+//  StarTrack
+//
+//  Created by Aluno 14 on 6/25/25.
+//
+
+import Foundation
---
+++ StarTrack/StarTrack/View/Perfil/PerfilView.swift
@@ -0,0 +1,20 @@
+//
+//  PerfilView.swift
+//  StarTrack
+//
+//  Created by Aluno 14 on 6/25/25.
+//
+
+import SwiftUI
+
+struct PerfilView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PerfilView_Previews: PreviewProvider {
+    static var previews: some View {
+        PerfilView()
+    }
+}
