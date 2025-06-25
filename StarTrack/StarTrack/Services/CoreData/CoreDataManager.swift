8cb39791e00db3c5e36374ee43369abebaffef03
Gabriel Rugeri <gabriel.rugeri@outlook.com>
2025-06-25 12:15:06 -0300

    CD manager placeholder

---
+++ StarTrack/StarTrack/Services/CoreData/CoreDataManager.swift
@@ -0,0 +1,22 @@
+//
+//  CoreDataManager.swift
+//  StarTrack
+//
+//  Created by Gabriel Rugeri on 25/06/25.
+//
+
+import Foundation
+import CoreData
+
+// MARK: - Gestor de Dados do Core Data
+// Esta classe é a única que interage diretamente com o Core Data.
+class CoreDataManager {
+	
+	private let context: NSManagedObjectContext
+	
+	init(context: NSManagedObjectContext) {
+		self.context = context
+	}
+	
+	/// inserir funcoes fetch()
+}
