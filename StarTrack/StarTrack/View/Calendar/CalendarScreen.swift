382ca66d988d16b7d5bb5e4b4fe6df68341b358e
Fábio Ribeiro <104939520+Fab1o98@users.noreply.github.com>
2025-06-25 11:08:07 -0300

    Pagina de calendario e favoritos, faltando atualização dos favoritos

---
+++ StarTrack/StarTrack/View/Calendar/CalendarScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CalendarScreen.swift
+//  StarTrack
+//
+//  Created by Aluno 10 on 25/06/25.
+//
+
+import SwiftUI
+
+struct CalendarScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CalendarScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarScreen()
+    }
+}
---
+++ StarTrack/StarTrack/View/Calendar/FavoritesView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  StarTrack
+//
+//  Created by Aluno 10 on 25/06/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
