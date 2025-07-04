f781de9a07d1775023b10486e78fa284b9efef6e
Gabriel Amaral <gabrielamaral1301@gmail.com>
2025-07-04 11:32:15 -0300

    Preenchendo os cards de acordo com cada evento na tela Calendário [WIP]

--- StarTrack/StarTrack/View/Calendar/CalendarView.swift
+++ StarTrack/StarTrack/View/Calendar/CalendarView.swift
@@ -10,55 +10,57 @@ import SwiftUI
 struct CardCalendar:View {
 //    var title = "Evento"
 //    var date: Date
+    let name: String
+    let imageName: String
     
-    
+    init(name: String, imageName: String){
+        self.name = name
+        self.imageName = imageName
+    }
+
     var body : some View{
-        ZStack(alignment:.leading){
-            
-            Image("earth")
-            //.resizable()
-                .frame(width: 344, height: 106)
+        ZStack(alignment:.leading) {
+            Image("\(imageName)")
+                .resizable()
+                .scaledToFill()
+                .frame(width: 350, height: 106)
                 .cornerRadius(10)
                 .clipped()
-                
-            
-            
+
+
             VStack(alignment: .leading){
-                
-                Text("Titulo")
+                Text("\(name)")
                     .font(.title2)
                     .foregroundColor(.white)
-                
-                
-                Text("Data")
-                    .font(.subheadline)
-                    .foregroundColor(.white)
-                
-                Text("Hora")
-                    .font(.subheadline)
-                    .foregroundColor(.white)
-                
+                    .padding(.leading, 12)
+            }
+//             Text("Data")
+//                .font(.subheadline)
+//                .foregroundColor(.white)
+//
+//             Text("Hora")
+//                .font(.subheadline)
+//                .foregroundColor(.white)
             }
             .padding(.horizontal,14)
-            
         }
     }
-}
+
 
 struct choicesUserView: View {
     @State private var selected = "Eventos"
     let options = ["Eventos", "Favoritos"]
-    
+
     var body: some View{
-        
-        
+
+
     Picker("", selection: $selected){
         ForEach(options, id: \.self) { option in
             Text(option)
         }
     }
     .pickerStyle(.segmented)
-    .frame(width: 344, height: 32, alignment: .center)
+    .frame(width: 350, height: 32, alignment: .center)
     .padding(.vertical,24)
 
 if selected == "Eventos" {
@@ -68,29 +70,36 @@ if selected == "Eventos" {
                     // Tela de Favoritos
                     FavoritesView()
                 }
-        
+
     }
 }
 
 struct ScrollCardView: View {
+    let events: [CalendarModel] = [
+        CalendarModel(name: "Alfa-Capricórnidas", imageName: "Alfa-Capricórnidas"),
+        CalendarModel(name: "Asteroide 63", imageName: "Asteroide 63"),
+        CalendarModel(name: "Lua e M45", imageName: "Lua e M45"),
+        CalendarModel(name: "Lua, Saturno e Netuno", imageName: "Lua, Saturno e Netuno"),
+        CalendarModel(name: "Piscis Austrinid", imageName: "Piscis Austrinid")
+    ]
     var body: some View{
         ScrollView {
             VStack {
-                ForEach(0..<6) { _ in
-                    CardCalendar()
+                ForEach(events) { event in
+                    CardCalendar(name: event.name, imageName: event.imageName)
                 }
-                .padding(.vertical, 12)
+                .padding(.vertical, 2)
             }
         }
     }
 }
 
 struct CalendarView: View {
-   
-    
+
+
     var body: some View {
         NavigationView{
-            
+
             VStack{
                     choicesUserView()
 
@@ -111,6 +120,7 @@ struct CalendarView: View {
         }
     }
 }
+
 
 struct CalendarView_Previews: PreviewProvider {
     static var previews: some View {
